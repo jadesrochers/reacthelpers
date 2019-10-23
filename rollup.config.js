4b7d2dc4def3182f8a0d99ad6c3412579401ae83
jad <jadesrochers@gmail.com>
2019-10-22 22:33:59 -0400

    Still working with external deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,24 @@
 import babel from 'rollup-plugin-babel';
+/* import autoExternal from 'rollup-plugin-auto-external'; */
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
     input: './index.js',
+    external: ['react'],
     output: {
         file: './dist/bundle.js',
         format: 'iife',
-        name: 'bundle'
+        name: 'bundle',
+        globals: {
+        'react': '_.React'
+      }
     },
     plugins: [
         babel({
             exclude: 'node_modules/**'
-        })
+        }),
+        resolve(),
+        commonjs()
     ]
 }
